a2d30b1e36bdae03b5c65ced8e6e05ebd810d330
minh <pesrobot@gmail.com>
2016-06-27 18:59:00 +0300

    adding js to dropdown nav

---
+++ js/main.js
@@ -0,0 +1,9 @@
+alert("I am an alert box!");
+/*
+jQuery(document).ready(function($){
+	console.log("jQuery is working");
+	$('#block-viku-main-menu li a').hover(function(){
+		$(this).parent().find('.sub-menu').toggle();
+	});	
+});
+*/
