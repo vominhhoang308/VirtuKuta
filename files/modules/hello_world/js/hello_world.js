a75d1e5355389e4bc2656693054a847e6ea4e442
minh <pesrobot@gmail.com>
2016-07-05 17:36:21 +0300

    group partially solved responsive body and footer, created new twig templates & content

---
+++ files/modules/hello_world/js/hello_world.js
@@ -0,0 +1,8 @@
+(function($) {
+    $(document).ready(function() {
+      console.log('hello');	 
+	   //console.log('hello');
+		//$('.title').draggable();
+		$('.block-title').text("My new title"); 
+    });
+}(jQuery));
